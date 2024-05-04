cd8608dd55eeb1c33e18ef79f44fb3620c0f7877
FRanggaY <franciscusrangga@gmail.com>
2024-05-04 15:49:55 +0700

    update : change name table

--- alembic/versions/c7d23a9d47b5_create_solution_table.py
+++ alembic/versions/c7d23a9d47b5_create_solution_table.py
@@ -1,8 +1,8 @@
-"""create service translation table
+"""create solution table
 
-Revision ID: edc2de8025ed
-Revises: c7d23a9d47b5
-Create Date: 2024-05-04 14:47:29.938333
+Revision ID: c7d23a9d47b5
+Revises: b2f6b54e465d
+Create Date: 2024-05-04 14:47:24.436193
 
 """
 from typing import Sequence, Union
@@ -13,25 +13,26 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'edc2de8025ed'
-down_revision: Union[str, None] = 'c7d23a9d47b5'
+revision: str = 'c7d23a9d47b5'
+down_revision: Union[str, None] = 'b2f6b54e465d'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.create_table(
-        'service_translations',
+        'solutions',
         sa.Column('id', sa.String(36), primary_key=True, server_default=str(uuid.uuid4())),
-        sa.Column('service_id', sa.String(36), nullable=True),
+        sa.Column('user_id', sa.String(36), nullable=True),
         sa.Column('title', sa.String(128), unique=False, nullable=False),
-        sa.Column('description', sa.String(512), unique=False, nullable=True),
-        sa.Column('language_id', sa.Enum('id', 'en'), nullable=True),
+        sa.Column('image_url', sa.String(512), unique=False, nullable=True),
+        sa.Column('logo_url', sa.String(512), unique=False, nullable=True),
+        sa.Column('is_active', sa.Boolean, default=True),
         sa.Column('created_at', sa.DateTime, server_default=sa.func.NOW(), nullable=False),
         sa.Column('updated_at', sa.DateTime, server_default=sa.func.NOW(), onupdate=sa.func.NOW(), nullable=False),
     )
-    op.create_foreign_key("fk_service_translation_service_id", 'service_translations', 'services',
-                        ["service_id"], ["id"], ondelete='CASCADE', onupdate='CASCADE')
+    op.create_foreign_key("fk_solution_user_id", 'solutions', 'users',
+                        ["user_id"], ["id"], ondelete='CASCADE', onupdate='CASCADE')
 
 def downgrade() -> None:
-    op.drop_table('service_translations')
+    op.drop_table('solutions')
--- alembic/versions/edc2de8025ed_create_solution_translation_table.py
+++ alembic/versions/edc2de8025ed_create_solution_translation_table.py
@@ -1,8 +1,8 @@
-"""create service table
+"""create solution translation table
 
-Revision ID: c7d23a9d47b5
-Revises: b2f6b54e465d
-Create Date: 2024-05-04 14:47:24.436193
+Revision ID: edc2de8025ed
+Revises: c7d23a9d47b5
+Create Date: 2024-05-04 14:47:29.938333
 
 """
 from typing import Sequence, Union
@@ -13,26 +13,25 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c7d23a9d47b5'
-down_revision: Union[str, None] = 'b2f6b54e465d'
+revision: str = 'edc2de8025ed'
+down_revision: Union[str, None] = 'c7d23a9d47b5'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.create_table(
-        'services',
+        'solution_translations',
         sa.Column('id', sa.String(36), primary_key=True, server_default=str(uuid.uuid4())),
-        sa.Column('user_id', sa.String(36), nullable=True),
+        sa.Column('solution_id', sa.String(36), nullable=True),
         sa.Column('title', sa.String(128), unique=False, nullable=False),
-        sa.Column('image_url', sa.String(512), unique=False, nullable=True),
-        sa.Column('logo_url', sa.String(512), unique=False, nullable=True),
-        sa.Column('is_active', sa.Boolean, default=True),
+        sa.Column('description', sa.String(512), unique=False, nullable=True),
+        sa.Column('language_id', sa.Enum('id', 'en'), nullable=True),
         sa.Column('created_at', sa.DateTime, server_default=sa.func.NOW(), nullable=False),
         sa.Column('updated_at', sa.DateTime, server_default=sa.func.NOW(), onupdate=sa.func.NOW(), nullable=False),
     )
-    op.create_foreign_key("fk_service_user_id", 'services', 'users',
-                        ["user_id"], ["id"], ondelete='CASCADE', onupdate='CASCADE')
+    op.create_foreign_key("fk_solution_translation_solution_id", 'solution_translations', 'solutions',
+                        ["solution_id"], ["id"], ondelete='CASCADE', onupdate='CASCADE')
 
 def downgrade() -> None:
-    op.drop_table('services')
+    op.drop_table('solution_translations')
